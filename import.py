3003073e6aba0a6e46c7e1c346fa00ce77c4fd8f
Artem <mail@artempal.ru>
2020-03-31 17:59:51 +0300

    скрипты первоначальной настройки и импорта

--- import.py
+++ import.py
@@ -6,42 +6,65 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'waf.settings')
 django.setup()
 from main.models import BlackList, AttackType
 
-workbook = load_workbook('data.xlsx', read_only=True)
-first_sheet = workbook.sheetnames[0]
-worksheet = workbook[first_sheet]
 
-i = 0
-attack_type = AttackType.objects
-for row in worksheet.iter_rows():
-    i += 1
-    if i == 1:  # пропускаем заголовок таблицы
-        continue
-    blacklist = BlackList()
-    math_zone = row[5].value
-    reg_type = row[1].value
-    reg = row[2].value
-    tag = row[3].value
-    if math_zone.find('BODY') != -1:
-        blacklist.body = True
-    if math_zone.find('HEADERS') != -1:
-        if math_zone.find('$HEADERS_VAR') != -1:
-            blacklist.head = False
-        else:
-            blacklist.head = True
-    if math_zone.find('URL') != -1:
-        if math_zone.find('$URL') != -1:
-            blacklist.url = False
-        else:
-            blacklist.url = True
-    if math_zone.find('ARGS') != -1:
-        blacklist.args = True
+def clear_all():
+    BlackList.objects.all().delete()
+    AttackType.objects.all().delete()
 
-    blacklist.active = True
 
-    if reg_type == 'RLx':
-        blacklist.stable = False
-    elif reg_type == 'RL':
-        blacklist.stable = True
-    blacklist.reg = reg
-    blacklist.type = attack_type.get(name=tag)
-    blacklist.save()
+def create_attack_type():
+    AttackType.objects.bulk_create([AttackType(1, "Injection"),
+                                    AttackType(2, "UWA"),
+                                    AttackType(3, "Scanner"),
+                                    AttackType(4, "Other"),
+                                    AttackType(5, "XSS"),
+                                    AttackType(6, "Evasion")])
+
+
+def import_table():
+    workbook = load_workbook('data.xlsx', read_only=True)
+    first_sheet = workbook.sheetnames[0]
+    worksheet = workbook[first_sheet]
+
+    i = 0
+
+    attack_type = AttackType.objects
+    for row in worksheet.iter_rows():
+        i += 1
+        if i == 1:  # пропускаем заголовок таблицы
+            continue
+        blacklist = BlackList()
+        math_zone = row[5].value
+        reg_type = row[1].value
+        reg = row[2].value
+        tag = row[3].value
+        if math_zone.find('BODY') != -1:
+            blacklist.body = True
+        if math_zone.find('HEADERS') != -1:
+            if math_zone.find('$HEADERS_VAR') != -1:
+                blacklist.head = False
+            else:
+                blacklist.head = True
+        if math_zone.find('URL') != -1:
+            if math_zone.find('$URL') != -1:
+                blacklist.url = False
+            else:
+                blacklist.url = True
+        if math_zone.find('ARGS') != -1:
+            blacklist.args = True
+
+        blacklist.active = True
+
+        if reg_type == 'RLx':
+            blacklist.stable = False
+        elif reg_type == 'RL':
+            blacklist.stable = True
+        blacklist.reg = reg
+        blacklist.type = attack_type.get(name=tag)
+        blacklist.save()
+
+
+if __name__ == "__main__":
+    clear_all()
+    create_attack_type()
+    import_table()
---
+++ main/management/commands/configuration.py
@@ -0,0 +1,26 @@
+from main.models import Configs
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    help = 'Предварительная настройка системы'
+
+    def add_arguments(self, parser):
+        parser.add_argument('hostname', type=str)
+        parser.add_argument('port', type=int)
+        parser.add_argument('https', type=bool)
+
+    def handle(self, *args, **options):
+        if options['hostname'] and options['port'] and options['https'] is not None:
+            if not Configs.objects.filter(pk=1).exists():  # если есть, то редактируем
+                configs = Configs()
+            else:
+                configs = Configs.objects.get(pk=1)
+            configs.hostname = options['hostname']
+            configs.port = options['port']
+            configs.https = options['https']
+            configs.save()
+        else:
+            raise CommandError('Не переданы необходимые параметры')
+
+        self.stdout.write(self.style.SUCCESS('Готово!'))
